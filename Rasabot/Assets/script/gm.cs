a048802ef4b10fe16d4eb7b7c96b795e46c58b59
m10913044 <87231581+m10913044@users.noreply.github.com>
2021-09-29 23:46:22 +0800

    testgame

--- Rasabot/Assets/script/gm.cs
+++ Rasabot/Assets/script/gm.cs
@@ -2,7 +2,23 @@
 //using System.Collections.Generic;
 using UnityEngine;
 
+
 public class gm : MonoBehaviour
 {
-    
+    public Transform School;
+    public Transform Park; 
+    public Transform Library; 
+    public GameObject CarControl; 
+    public void Start() { 
+        CarControl = GameObject.Find("yunbot"); 
+    }
+    public void GoSchool() { 
+        CarControl.GetComponent<Pathfinding.AIDestinationSetter>().target = School.transform; 
+    }
+    public void GoPark() { 
+        CarControl.GetComponent<Pathfinding.AIDestinationSetter>().target = Park.transform; 
+    }
+    public void GoLibrary() { 
+        CarControl.GetComponent<Pathfinding.AIDestinationSetter>().target = Library.transform; 
+    }
 }
